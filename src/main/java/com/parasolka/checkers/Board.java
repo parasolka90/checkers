74b3e23fe25934d74d30ef522f9321efd7334732
parasolka90 <parasolka90@gmail.com>
2017-08-01 22:35:11 +0200

    hit method - refactoring

--- src/main/java/com/parasolka/checkers/Board.java
+++ src/main/java/com/parasolka/checkers/Board.java
@@ -80,10 +80,10 @@ public class Board {
     }
 
     public boolean hit(int x0, int y0, int x1, int y1) {
-        if (x1-1 <= 0 || x1+1 >=size) {
+        if (x1 - 1 <= 0 || x1 + 1 >= size) {
             return false;
         }
-        if (y1-1 <= 0 || y1+1 >= size) {
+        if (y1 - 1 <= 0 || y1 + 1 >= size) {
             return false;
         }
         String beatingPawn = getFigure(x0, y0);
@@ -103,16 +103,16 @@ public class Board {
                 setFigure(new Pawn("_"), x0, y0);
                 setFigure(new Pawn("_"), x1, y1);
                 setFigure(new Pawn(beatingPawn), x0 + 2, y0 + 2);
-        return true;
-        } else if (x0 - 1 == x1 && y0 + 1 == y1 && getFigure(x0 - 2, y0 + 2) == "_") {
-        setFigure(new Pawn("_"), x0, y0);
-        setFigure(new Pawn("_"), x1, y1);
-        setFigure(new Pawn(beatingPawn), x0 - 2, y0 + 2);
-        return true;
-        }
+                return true;
+            } else if (x0 - 1 == x1 && y0 + 1 == y1 && getFigure(x0 - 2, y0 + 2) == "_") {
+                setFigure(new Pawn("_"), x0, y0);
+                setFigure(new Pawn("_"), x1, y1);
+                setFigure(new Pawn(beatingPawn), x0 - 2, y0 + 2);
+                return true;
+            }
         }
         return false;
-        }
+    }
 
-        }
+}
 
